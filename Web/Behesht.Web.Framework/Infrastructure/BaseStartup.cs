b8ed44b10d08d1ca38e6f8903e15b74e839a4a27
amir_beh71 <amirbeh71@gmail.com>
2022-06-06 15:19:00 +0430

    latest

--- Web/Behesht.Web.Framework/Infrastructure/BaseStartup.cs
+++ Web/Behesht.Web.Framework/Infrastructure/BaseStartup.cs
@@ -1,7 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.OpenApi.Models;
 using System;
 using System.Collections.Generic;
